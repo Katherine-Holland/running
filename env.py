7c13736ad9619f2115a2118cdfa3b13ad7cd8f6f
Katherine Weston <littlerobinagency@gmail.com>
2024-08-26 20:02:36 +0000

    deployment settings

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://uz2lhudnoiz:sthHFXvIqHp2@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/stand_stung_thong_850125")
