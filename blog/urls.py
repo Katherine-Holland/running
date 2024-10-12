8533d0e475af81809987ebc41c28a6c612d91d03
Katherine Weston <littlerobinagency@gmail.com>
2024-10-12 19:46:04 +0000

    Add url paths to blog app and running app

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from . import views
+from django.urls import path, include
+
+from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path("", views.PostList.as_view(), name='home'),
 ]
