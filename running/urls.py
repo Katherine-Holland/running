541913f366db677efc585bbb7e87424ebe5c6dd9
Katherine Weston <littlerobinagency@gmail.com>
2024-10-22 20:45:19 +0000

    add about image

--- running/urls.py
+++ running/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
-    path('', include('home.urls')),  # Includes home.urls at the root
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('', include('home.urls')),    
     path('summernote/', include('django_summernote.urls')),
-    path('blog/', include('blog.urls')),  
 ]
