1839c55e60c29883aed9d6eab2d2d0dbe58e74c1
Katherine Weston <littlerobinagency@gmail.com>
2024-10-28 20:20:57 +0000

    add docstrings to project

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
 class PostAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fileds for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+    
     list_display = ('title', 'slug', 'status')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on',)
