ff9fcbad2bb6ef2fd308013b460219158cf4d591
Katherine Weston <littlerobinagency@gmail.com>
2024-10-28 21:08:45 +0000

    amend the settings/py file to Debug False

--- running/settings.py
+++ running/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-katherinehollan-running-kzyuolq53tz.ws-eu116.gitpod.io', 'trailrunning-f565347cbca0.herokuapp.com', '.herokuapp.com']
 
