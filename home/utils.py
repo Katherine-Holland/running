9b005c675d9152fef66abf47061dd8a332c41127
Katherine Weston <littlerobinagency@gmail.com>
2024-10-31 22:42:56 +0000

    add weather api to home html

---
+++ home/utils.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import requests
+import os
+
+def get_weather_data(city, country):
+    api_key = os.environ.get("OPENWEATHER_API_KEY")
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric"
+
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+        icon_code = weather_data['weather'][0]['icon']
+        weather_data['icon_code'] = icon_code
+        return weather_data
+    else:
+        return None
+
+
+def get_greeting():
+    current_hour = datetime.now().hour
+    if 5 <= current_hour < 12:
+        return "Good morning!"
+    elif 12 <= current_hour < 18:
+        return "Good afternoon!"
+    else:
+        return "Good evening!"
+
+
+def home_view(request):
+    greeting = get_greeting()
+    weather_data = get_weather_data("Kendal", "GB")
+    
+    return render(request, 'home/home.html', {'greeting': greeting, 'weather_data': weather_data})
--- home/views.py
+++ home/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .utils import get_greeting, get_weather_data  # Ensure both functions are imported
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    # Get greeting based on the current time
+    greeting = get_greeting()
+    
+    # Fetch weather data for a specific city
+    city = "Kendal"  # You can make this dynamic based on user input or location
+    weather = get_weather_data(city, "GB")  # Adjusted to include country code
+    
+    # Render the template with both greeting and weather data
+    return render(request, 'home/home.html', {
+        'greeting': greeting,
+        'weather': weather
+    })
+
+def get_greeting():
+    current_hour = timezone.now().hour
+    if current_hour < 12:
+        return "Good morning!"
+    elif 12 <= current_hour < 18:
+        return "Good afternoon!"
+    else:
+        return "Good evening!"
